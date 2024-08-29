079c5dd2f76d822d1bc89a177c828f60bcc9c57b
Jav2004ier <156093114+Jav2004ier@users.noreply.github.com>
2024-08-29 10:59:47 -0500

    Cambios

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,6 +13,9 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideHttpClient(), 
-    provideFirebaseApp(() => initializeApp({"projectId":"app-pokemon-21683","appId":"1:571738223421:web:a781203b1998db1715cb54","storageBucket":"app-pokemon-21683.appspot.com","apiKey":"AIzaSyCxD0w629vJDjVcW8sBwlvrtpLsFPdT7r8","authDomain":"app-pokemon-21683.firebaseapp.com","messagingSenderId":"571738223421"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideStorage(() => getStorage()),
+    provideFirebaseApp(() => initializeApp({"projectId":"app-pokemon-21683","appId":"1:571738223421:web:a781203b1998db1715cb54","storageBucket":"app-pokemon-21683.appspot.com","apiKey":"AIzaSyCxD0w629vJDjVcW8sBwlvrtpLsFPdT7r8","authDomain":"app-pokemon-21683.firebaseapp.com","messagingSenderId":"571738223421"})), 
+    provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, 
+    provideFirestore(() => getFirestore()),
+     provideStorage(() => getStorage()),
   ]
 };
